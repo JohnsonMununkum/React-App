2486ec0b3c0c6fee5e864ff72d106bcf4f5dcd93
Johnson <G00419319@atu.ie>
2024-10-04 10:33:16 +0000

    exercise2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <h1></h1>
+      <h1>Hello World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
