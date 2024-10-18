b4a830526b3840d8b3391b0639a38192e744afb8
Johnson <G00419319@atu.ie>
2024-10-18 11:19:41 +0000

    Finished project

--- src/Create.js
+++ src/Create.js
@@ -7,36 +7,60 @@ import { useState } from "react";
 //function to create & set the tile of the movies 
 const Create = () => {
 
-    const [title, setTitle] = useState('');
-//to handle the submit
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
+
+  //to handle the submit
   //adds the title to the page 
   const handleSubmit = (e) => {
-    e.preventdefault();
-    console.log(title);
+    e.preventDefault();
+    const movieData = {
+      title,
+      year,
+      poster,
+    };
+    console.log(movieData);
+
   }
 
-    return (
-      <div>
+  //inputting title year & poster
+  return (
+    <div>
 
-              <h3>Hello from the Create component</h3>
-              <form onSubmit ={handleSubmit}>
+      <h3>Hello from the Create component</h3>
+      <form onSubmit={handleSubmit}>
 
-                <div className="form-group">
-                  <label>Add Movie Title: </label>
-                  <input type="text"
-                    className="form-control"
-                    value={title}
-                    onChange={(e) => { setTitle(e.target.value) }} 
-                  />
-                </div>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+
+          <label> Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+
+          <label> Add Movie Poster: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
 
 
-                <div>
-                  <input type="submit" value="Add Movie"></input>
-                </div>
-              </form>
+        <div>
+          <input type="submit" value="Add Movie"></input>
+        </div>
+      </form>
     </div>
-    );
-  };
-    
-  export default Create;
+  );
+};
+
+export default Create;
