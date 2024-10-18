b8ffed13ad3c74fd562cd9ed637b481b4574ca50
Johnson <G00419319@atu.ie>
2024-10-18 11:00:11 +0000

    Add movie page

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import { useState } from "react";
+
 
 //footer.js
 //create reuseable Create component
 //can be imported into the app.js file 
+//function to create & set the tile of the movies 
 const Create = () => {
-    return <h3>Hello from the Create component</h3>;
+
+    const [title, setTitle] = useState('');
+//to handle the submit
+  //adds the title to the page 
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    console.log(title);
+  }
+
+    return (
+      <div>
+
+              <h3>Hello from the Create component</h3>
+              <form onSubmit ={handleSubmit}>
+
+                <div className="form-group">
+                  <label>Add Movie Title: </label>
+                  <input type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }} 
+                  />
+                </div>
+
+
+                <div>
+                  <input type="submit" value="Add Movie"></input>
+                </div>
+              </form>
+    </div>
+    );
   };
-  
+    
   export default Create;
