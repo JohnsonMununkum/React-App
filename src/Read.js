2e7582278edf0bd9c4dda5cffc9d4cca5f6445a5
Johnson <G00419319@atu.ie>
2024-10-18 10:39:29 +0000

    app working with useState

--- src/Read.js
+++ src/Read.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import Movies from './Movies';
 //importing movies into read.js
 
@@ -11,7 +12,7 @@ const Read = () => {
 
     //array of movies being put in
     const data = [
-        {
+      /*  {
           "Title": "Avengers: Infinity War",
           "Year": "2018",
           "imdbID": "tt4154756",
@@ -31,14 +32,40 @@ const Read = () => {
           "imdbID": "tt0816711",
           "Type": "movie",
           "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
+        }*/
       ];
+
+      //estate variable called movies given an initial value with an empty array
+      const [movies, setMovies] = useState([]);
+
+      //get api from external source 
+      //synchronise with an external system
+      useEffect(
+        ()=>{
+            //use a package call axis
+            //app now has abillity to http methods
+            //axios.get makes a http request to our api doing it in the background
+            //app will continue to work while this is going on
+            //result of this is called a promise
+            axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+            //.then this will happen succesfully
+            //callbackfunctions
+            //execute .then when it has excuted successfully
+            .then((response)=> {
+           console.log(response.data);
+           //passed an array
+           setMovies(response.data.movies);
+            })
+            //,catch if something goes wrong 
+            .catch()
+        }
+      );
       
             //passing array of data down to movies
     return(
         <div>
             <h3>Hello from the Read component</h3>
-            <h4><Movies myMovies={data}/> </h4>
+            <h4><Movies myMovies={movies}/> </h4>
         </div>
     );
   };
