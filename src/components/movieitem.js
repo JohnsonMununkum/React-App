c6f3bebac4ceaadf940d3b155fe4791be68438dc
Johnson <G00419319@atu.ie>
2024-11-22 12:07:38 +0000

    added the button in the movie item

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import Card from 'react-bootstrap/Card';
+//navigate to a new route without refreshing the page.
+import { Link } from 'react-router-dom';
 
+//defines the path the Link component should navigate to when clicked.
+//This URL structure matches the route defined in App.js (/edit/:id) and allows React Router to capture the movie’s ID and pass it to the Edit component
 const MovieItem = (props)=> {
   useEffect(() => {
     console.log("Movie Item:", props.mymovie);
@@ -15,7 +19,9 @@ const MovieItem = (props)=> {
             <img src={props.mymovie.poster} alt={props.mymovie.title} />
             <footer>{props.mymovie.year}</footer>
           </blockquote>
+
         </Card.Body>
+        <Link to={"/edit/" + props.mymovie._id} className="btn btn-primary">Edit</Link>
       </Card>
     </div>
   );
