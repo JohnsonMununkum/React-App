47853bd466711dafe283c1d22a3247a8acd3918d
Johnson <G00419319@atu.ie>
2024-11-22 12:16:25 +0000

    added the button in the movie item

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Footer from './components/footer';
 import Content from './components/content';
 import Read from './components/read';
 import Create from './components/create';
+//imported edit so it is allowed to be used
+import Edit from  './components/edit';
 
+//This line adds a new route to the application’s router configuration
+//The route allows users to navigate to the Edit component when they want to edit a specific movie.
 function App() {
   return (
     <Router>
@@ -15,6 +19,7 @@ function App() {
         <Route path="/" element={<Content />} />
         <Route path="/read" element={<Read />} />
         <Route path="/create" element={<Create />} />
+        <Route path='/edit/:id' element={<Edit />} />
       </Routes>
       <Footer />
     </Router>
