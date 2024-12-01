9322c3c7ea019ab35510572205bb22de22e48d90
Johnson <G00419319@atu.ie>
2024-12-01 15:43:04 +0000

    delete button after each movie

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -2,10 +2,27 @@ import { useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 //navigate to a new route without refreshing the page.
 import { Link } from 'react-router-dom';
+//adding button to movie item.js
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
+
+
 
 //defines the path the Link component should navigate to when clicked.
 //This URL structure matches the route defined in App.js (/edit/:id) and allows React Router to capture the movie’s ID and pass it to the Edit component
 const MovieItem = (props)=> {
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios.delete('http://localhost:4000/api/movie/' + props.myMovie._id)
+        .then(() => {
+            props.Reload(); // Refresh the movie list after deletion
+        })
+        .catch((error) => {
+            console.error("Error deleting movie:", error);
+        });
+};
+
+
   useEffect(() => {
     console.log("Movie Item:", props.mymovie);
   }, [props.mymovie]); // Only run this effect when the mymovie prop changes
@@ -23,6 +40,10 @@ const MovieItem = (props)=> {
         </Card.Body>
         <Link to={"/edit/" + props.mymovie._id} className="btn btn-primary">Edit</Link>
       </Card>
+            {/* Other movie details */}
+            <Button variant="danger" onClick={handleDelete}>Delete</Button>
+
+
     </div>
   );
 }
